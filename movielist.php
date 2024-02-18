1b379d7b9a641d1169dc2bd89ca993e6b9003047
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-02-18 22:56:40 +0530

    updated list

--- movielist.php
+++ movielist.php
@@ -13,24 +13,47 @@ include("headder.php");
 
 <body>
     <div class="main">
-        <div style=" display: flex;flex-wrap: wrap; gap:10px;">
+        <div style="display: flex;flex-wrap: wrap; gap:30px;width:100%; align-items: center; justify-content:flex-start;">
             <?php
-            $sql = "SELECT * FROM movies";
-            $result = mysqli_query($conn, $sql);
-            while ($row = mysqli_fetch_array($result)) {
-                echo
-                "<a href='moviedetails.php?movieid={$row["movieid"]}'>
-                    <div class='card'> 
-                        <div class='image-box'>
-                            <img src='{$row["image_url"]}'>
-                         </div>
-                        <div class='content'>
-                            <h2>{$row["title"]}</h2>
-                            <p>{$row["description"]}</p>
+            if ($_SERVER["REQUEST_METHOD"] == "GET") {
+                $conditions = array();
+                if (isset($_GET["movieid"])) {
+                    $conditions[] = "movieid = " . $_GET["movieid"];
+                }
+                if (isset($_GET["title"])) {
+                    $conditions[] = "title LIKE '%" . $_GET["title"] . "%'";
+                }
+                if (isset($_GET["genre"])) {
+                    $conditions[] = "genre LIKE '%" . $_GET["genre"] . "%'";
+                }
+                if (isset($_GET["language"])) {
+                    $conditions[] = "language LIKE '%" . $_GET["language"] . "%'";
+                }
+                // Check if any conditions are added
+                if (!empty($conditions)) {
+                    $where_clause = " WHERE " . implode(" AND ", $conditions);
+                } else {
+                    $where_clause = "";
+                }
+                $sql = "SELECT * FROM movies" . $where_clause;
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_array($result)) {
+                    echo
+                    "<a href='moviedetails.php?movieid={$row["movieid"]}'>
+                        <div class='card'> 
+                            <div class='image-box'>
+                                <img src='{$row["image_url"]}'>
+                             </div>
+                            <div class='content'>
+                                <h2>{$row["title"]}</h2>
+                                <p>{$row["description"]}</p>
+                            </div>
                         </div>
-                    </div>
-                </a>";
+                    </a>";
+                }
             }
+
+
             ?>
         </div>
     </div>
